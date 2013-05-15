0915a70160b96dce477777584d1152ff210e705f
hieunv26 <hieunv26@gmail.com>
2013-05-15 14:36:43 +0700

    hieunv

--- urls.py
+++ urls.py
@@ -3,3 +3,14 @@ Created on May 13, 2013
 
 @author: rega
 '''
+from django.conf.urls import patterns, url, include
+import settings
+urlpatterns = patterns('',
+    url(r'^movie', include('apps.movies.urls', namespace='movies')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+#       (r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.STATIC_ROOT }),
+    )
