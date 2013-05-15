79a8bac1f62ce14f27fef4a14b5f9cad1cf6e155
duyvk <duyvk142@gmail.com>
2013-05-15 14:46:52 +0700

    a

--- utils/request_introspection_middleware.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from utils import log as logging
-
-class DumpRequestMiddleware:
-    def process_request(self, request):
-        if settings.DEBUG:
-            request_items = request.REQUEST.items()
-            if request_items:
-                logging.debug("~BC~FK%s" % dict(request_items))
