2b3caf3895640082cc7fc86adf1200c339dd5644
manhtuhtk <manhtu.htk@gmail.com>
2013-05-16 16:00:20 +0700

    init userinfo data structure

--- apps/userinfo/models.py
+++ apps/userinfo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, unique=True, related_name='profile')
+    go_account_id = models.CharField(max_length=10, blank=True, null=True)
