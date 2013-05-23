e54bbbb62686537ccd7a3e0efc7f0eca9ca91366
duyvk <duyvk142@gmail.com>
2013-05-23 15:39:47 +0700

    finish insert movie

--- apps/movies/insert_movies.py
+++ apps/movies/insert_movies.py
@@ -1,51 +1,209 @@
 '''
-Created on May 21, 2013
-
-@author: kidio
-'''
-'''
 Created on Mar 22, 2013
 
 @author: kidio
 '''
-import pyodbc as msdb
 
+import pyodbc as msdb
+from datetime import datetime
+from apps.movies.models import Movie, Genre, Region, Artist,GOTV_JOBS
 CLIP_CATE_CODE = 5
 MOVIE_CATE_COTE = 2
 MUSIC_MVCLIP_CATE_CODE = 4
 SOURCE_TYPE_CODE = 1
 
-cate_code = 0
-#MAke the MSSQL connection
-conn_ms = msdb.connect('DRIVER=FreeTDS;SERVER=117.103.196.39;PORT=1433;DATABASE=tv.go.vn;UID=tv.go.vn;PWD=goTV!@#;TDS_Version=8.0;')
-cursor_ms = conn_ms.cursor()
+def get_or_create_genre(genreId, genreName):
+    genre = Genre.objects.filter(genre_id=int(genreId))
+    if not genre:
+        genre = Genre.objects.create(genre_id=int(genreId),genre_name=genreName)
+        return genre
+    return genre[0]
 
- 
-# Lists the tables in demo
-
-myGetQuery_movie = "select  top 1 * from view_ListMovies"
-
-
-    # Execute the SQL query and get the response
-cursor_ms.execute(myGetQuery_movie)
-response = cursor_ms.fetchall()
- 
-list_colums = []
-for row in cursor_ms.columns(table='view_ListMovies'):
-    list_colums.append(str(row.column_name))
-print list_colums
-
-# Loop through the response and print table names
-for row in response:
-    for key, value in enumerate(row):
-        print list_colums[key]
-        print str(value)
-        print "-----"
+def get_or_create_artist(personName, hisJob):
+    artist = Artist.objects.filter(artist_name=personName,artist_job=hisJob)
+    if not artist:
+        artist = Artist.objects.create(artist_name=personName,artist_job=hisJob)
+        return artist
+    return artist[0]
 
 
+def get_or_create_movie( movieID, sourcetype):
+    movie = Movie.objects.filter(movie_id=int(movieID),source_type=sourcetype)
+    if not movie:
+        movie = Movie.objects.create(movie_id=int(movieID),source_type=sourcetype)
+        return movie
+    return movie[0]
+
+
+def get_or_create_region(id,name):
+    region = Region.objects.filter(region_id=id,region_name=name)
+    if not region:
+        region = Region.objects.create(region_id=id,region_name=name)
+        return region
+    return region[0]
+
+
+def convert_str_2_list(datum, separator, clazz):
+    """
+    @param datum: str
+    @param separator: str
+    @param clazz: int or str or anything convertable
+    """
+    list_fields = []
+    if datum:
+        list_fields = datum.split(separator)
+    fields = []
+    for field in list_fields:
+        temp = field.strip()
+        if temp and temp.upper() != 'NULL':
+            if temp[0] == '[' and temp[-1] == ']':
+                temp = temp[1:-1]
+            fields.append(clazz(temp))
+    return fields
+
+def to_list_int(datum, separator=','):
+    return convert_str_2_list(datum, separator, int)
+
+def to_list_string(datum, separator=','):
+    return convert_str_2_list(datum, separator, unicode)
+
+
+def insert_one_movie_row(movie_dict):
+
+    try:
+        movieID = int(movie_dict['Id'])
+        sourcetype = int(movie_dict['SourceType'])
+        #movie = Movie(movie_id=movieID, source_type=sourcetype)
+        movie=get_or_create_movie(movieID, sourcetype)
+
+        if   "CategoryId" in movie_dict:
+            movie.cate = int(str(movie_dict['CategoryId']).strip())
+        
+        if "Name" in movie_dict:
+            movie.movie_name = movie_dict["Name"]
+            movie.save()
+#        
+        if "SubCategoryId" in movie_dict and "SubCategoryName" in movie_dict:
+            movie.subcates_name = str(movie_dict['SubCategoryName']).strip()
+            #movie.save()
+            sub_cate_dict = dict(zip(to_list_int(movie_dict['SubCategoryId'],','), to_list_string(movie_dict['SubCategoryName'],',')))
+            for key in sub_cate_dict:
+                temp = get_or_create_genre(key,sub_cate_dict[key])
+                movie.subcates.add(temp)
+
+        
+#
+##        if "ManufacturerName" in movie_dict:
+##            movie.ManufacturerName = movie_dict["ManufacturerName"]
+#
+        if "Imdb" in movie_dict:
+            movie.IMDB_score = movie_dict["Imdb"]
+            #movie.save()
+        
+        if "direction" in movie_dict:
+            directors = to_list_string(movie_dict["direction"])
+            for director in directors:
+                movie.directors.add(get_or_create_artist(director, "DIRECTOR"))
+            #movie.save()
+        
+        
+        if "Actor" in movie_dict:
+            casts = to_list_string(movie_dict["Actor"].strip())
+            for p in casts:
+                movie.casts.add(get_or_create_artist(p, "CAST"))
+        
+        if "LocationId" in movie_dict and  "LocationName" in movie_dict:
+            movie.locatio_name = str(movie_dict["LocationName"]).strip()
+            movie.location = get_or_create_region(int(movie_dict["LocationId"]),movie_dict["LocationName"])
+            
+        if "IsHd" in movie_dict:
+            if movie_dict["IsHd"] ==1:
+                movie.is_HD = True
+            else :
+                movie.is_HD = False
+        
+        if "Payment" in movie_dict:
+            isPay = int(str(movie_dict["Payment"]).strip())
+            if isPay == 0:
+                movie.is_free = True
+            else:
+                movie.is_free = False
+            #movie.save()
+            
+            
+        if "ReleaseDate" in movie_dict:
+            movie.release_date = datetime.strptime(str(movie_dict["ReleaseDate"]).split(" ")[0], "%Y-%m-%d")
+            #movie.save()
+            
+        if "ReleaseYear" in movie_dict:
+            movie.release_year = int (str(movie_dict["ReleaseYear"]).strip())
+
+        if "ImageUrl" in movie_dict:
+            movie.image_url =  str(movie_dict["ImageUrl"]).strip()
+
+        if "MovieTypeName" in movie_dict:
+            movie.movie_type = str(movie_dict['MovieTypeName']).strip()
+        
+        if "MovieTypeId" in movie_dict:
+            mtype = int(str(movie_dict["MovieTypeId"]).strip())
+            if mtype == 1:
+                movie.is_single_movie = True
+            else:
+                movie.is_single_movie = False
+                
+        if "ViewCount" in movie_dict:
+            movie.view_count = int(str(movie_dict["ViewCount"]).strip())
+
+        movie.save()
+    except:
+        pass
+
+
+def import_movie():
+
+    #MAke the MSSQL connection
+    conn_ms = msdb.connect('DRIVER=FreeTDS;SERVER=117.103.196.39;PORT=1433;DATABASE=tv.go.vn;UID=tv.go.vn;PWD=goTV!@#;TDS_Version=8.0;')
+    cursor_ms = conn_ms.cursor()
+
+    # Lists the tables in demo
+    myGetQuery_movie = "select  * from view_ListMovies"
+
+
+        # Execute the SQL query and get the response
+    cursor_ms.execute(myGetQuery_movie)
+    response = cursor_ms.fetchall()
+
+    list_colums = []
+    for row in cursor_ms.columns(table='view_ListMovies'):
+        list_colums.append(str(row.column_name))
+    print '"\n"'.join(list_colums)
+
+    # Loop through the response and print table names
+    for row in response:
+        
+        movie_dict = dict(zip(list_colums,row))
+    #    dicta = dict((list_colums[key],str(value))for key, value in enumerate(row))
+    #        print list_colums[key]
+    #        print str(value)
+    #        print "-----"
+    #    print dicto
+#        print movie_dict["Id"]
+#        print movie_dict["SourceType"]
+#        print movie_dict
+#        insert_one_movie_row(movie_dict)
+#        movie = get_or_create_movie(83 , 1)
+#        print "---"
+#        print movie
+#        print "cate: %s"% movie.cate
+#        print "subcate:"
+#        print movie.subcates_name
+#        print "name"
+#        print movie.movie_name
+#        print "imdb"
+#        print movie.IMDB_score
+        
+    conn_ms.close()
+
+if __name__ == '__main__':
+    import_movie()
     
-conn_ms.close()
-    
-
-
-
--- apps/movies/models.py
+++ apps/movies/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from datetime import datetime
-from raven.conf.defaults import MAX_LENGTH_LIST
-
-
-GOTV_MEDIA_TYPE=(
-    ('movie', _('MOVIE')),
-    ('mv', _('MOVIE_CLIP')),
-    ('clip', _('CLIP')),
-)
+#from django.utils.translation import ugettext as _
+#GOTV_MEDIA_TYPE=(
+#    ('movie', ('MOVIE')),
+#    ('mv', ('MOVIE_CLIP')),
+#    ('clip', ('CLIP')),
+#)
 
 GOTV_JOBS=(
     ('director', 'DIRECTOR'),
@@ -17,149 +15,176 @@ GOTV_JOBS=(
 )
 
 
+class Genre(models.Model):
+    """
+    Movie: subcategoryID
+    MovieClip, Clip: genreID
+    """
+    genre_id = models.IntegerField(primary_key=True)
+    genre_name = models.CharField(max_length=50, blank=True)
+
+    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
+
+    def __unicode__(self):
+        if self.genre_name and self.genre_id:
+            return u'%d: %s' % (self.genre_id, self.genre_name)
+        else :
+            return self.genre_id
+
+
+    class Meta:
+        app_label = 'movies'
+
+#class Manufacturer(models.Model):
+#    """
+#    Movie: Manufacturer
+#    MovieClip, Clip: Publisher
+#    """
+#    manu_id = models.AutoField(primary_key=True)
+#    manufacturer = models.CharField(max_length=200, db_index=True)
+#
+#    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
+#
+#    def __unicode__(self):
+#        return u'%s' % (self.manufacturer)
+#
+#    def get_or_create_manufacturer(self,manufacturerName):
+#        manufac = Manufacturer.objects.filter(manufacturer=manufacturerName)
+#        if not manufac:
+#            manufac = Manufacturer.objects.create(manufacturer=manufacturerName)
+#            return manufac
+#        return manufac[0]
+#
+#    class Meta:
+#        app_label = 'movies'
+
+class Artist(models.Model):
+    """
+    Movie: requires `person_name`, MovieClip also requires person_name
+    MovieClip: requires `personID`
+    """
+    artist_name = models.CharField(max_length=200, db_index=True,
+        help_text=('required by Movie/MovieClip/Clip'))
+
+    #person_id = models.AutoField(primary_key=True)
+
+    artist_job = models.CharField(max_length=40, db_index=True,null=True)
+
+    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
+
+    def __unicode__(self):
+        return (self.artist_name)
+
+    @models.permalink
+    def get_absolute_url(self):
+        pass
+    #    return ('people',(),{'pk':self.personId})
+
+
+
+    class Meta:
+        app_label = 'movies'
+
+class Region(models.Model):
+    """
+    Movie: LocationID
+    MovieClip, Clip: RegionID
+    """
+    region_id = models.IntegerField(primary_key=True)
+    region_name = models.CharField(max_length=200, blank=True)
+
+    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
+
+    def __unicode__(self):
+        return self.region_name
+
+
+
+    class Meta:
+        app_label = 'movies'
+
+
 class Movie(models.Model):
     '''
     Movie table
     '''
     movie_id = models.IntegerField(db_index=True)
-    source_type = models.IntegerField()
-    
-    movie_name = models.CharField(max_length=100, blank=True)
-    
-    subcates = models.ManyToManyField(GenreID, blank=True, null=True)
-    subcates_name = models.CharField(max_length=100, blank=True)
+    source_type = models.IntegerField(db_index=True)
+
+    movie_name = models.CharField(max_length=100, blank=True, null=True)
+
+    cate = models.IntegerField(null=True)
+
+    subcates = models.ManyToManyField(Genre, blank=True, null=True)
+    subcates_name = models.CharField(max_length=100, blank=True, null=True)
     #manufacturers = models.ManyToManyField(Manufacturer, blank=True, null=True)
 
-    
-    directors = models.ManyToManyField(People, blank=True, null=True,
+
+    directors = models.ManyToManyField(Artist, blank=True, null=True,
                                        related_name='%(app_label)s_%(class)s_directed')
-    casts = models.ManyToManyField(People, blank=True, null=True,
+    casts = models.ManyToManyField(Artist, blank=True, null=True,
                                    related_name='%(app_label)s_%(class)s_acted')
 
     #size = models.IntegerField(default=0, verbose_name=_('movie duration'), help_text=_('movie duration'))
 
-   
-    location = models.ForeignKey(RegionID, blank=True, null=True, on_delete=models.SET_NULL)
-    
+
+    location = models.ForeignKey(Region, blank=True, null=True, on_delete=models.SET_NULL)
+    location_name = models.CharField(null=True,max_length=20)
     IMDB_score = models.FloatField(blank=True, null=True)
-    
-    release_date = models.DateTimeField()
+
+    release_date = models.DateTimeField( null=True)
     release_year = models.IntegerField(blank=True, null=True)
 
     is_HD = models.BooleanField(default=False)
     is_free = models.BooleanField(default=True)
     is_single_movie = models.BooleanField(default=True)
 
-    movie_type = models.CharField(max_length=20, blank=True, null=True, choices=GOTV_MOVIE_TYPE)
-    
+    movie_type = models.CharField(max_length=40, blank=True, null=True)
+
     #tags = models.ManyToManyField(Tag, blank=True, null=True)
 
     # added
-    image_url = models.CharField(max_length=200, blank=True)
+    image_url = models.CharField(max_length=200, blank=True, null=True)
 
     # added
     view_count = models.IntegerField(default=0)
 
     #last_modified = models.DateTimeField(auto_now=True)
 
-    
+    def __unicode__(self):
+        name = unicode (self.movie_id)
+        if self.movie_name:
+            name = self.movie_name
+        return u'%s'% name
 
-class GenreID(models.Model):
-    """
-    Movie: subcategoryID
-    MovieClip, Clip: genreID
-    """
-    genreID = models.IntegerField(db_index=True)
-    
-    genre_name = models.CharField(max_length=50, blank=True)
-    
-    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
-    
-    def __unicode__(self):
-        name = unicode(self.genreID)
-        if self.genre_name:
-            name = self.genre_name
-        return u'%s_%s' % (name, self.media)
-    
-class Manufacturer(models.Model):
-    """
-    Movie: Manufacturer
-    MovieClip, Clip: Publisher
-    """
-    manufacturer = models.CharField(max_length=200, db_index=True)
-    
-    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
-    
-    def __unicode__(self):
-        return u'%s_%s' % (self.manufacturer, self.media)
-    
-class People(models.Model):
-    """
-    Movie: requires `person_name`, MovieClip also requires person_name
-    MovieClip: requires `personID`
-    """
-    person_name = models.CharField(max_length=200, db_index=True,
-        help_text=_('required by Movie/MovieClip/Clip'))
-    
-    personID = models.IntegerField(blank=True, null=True, db_index=True,
-        help_text=_('person name should be associated with a ID'))
-    
-    job = models.CharField(max_length=20, choices=GOTV_JOBS, db_index=True)
-    
-    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
-    
-    def __unicode__(self):
-        return u'%s' % (self.person_name)
-    
+#    def save(self, *args, **kwargs):
+#        movie = Movie.objects.filter(movie_id=self.movie_id)\
+#            .filter(source_type=self.source_type).exists()
+#        if not movie:
+#            super(Movie, self).save(self, *args, **kwargs)
+#            return True
+#        return False
+#
+
+
     @models.permalink
     def get_absolute_url(self):
         pass
-    #    return ('people',(),{'pk':self.personId})
-    
-    
-class RegionID(models.Model):
-    """
-    Movie: LocationID
-    MovieClip, Clip: RegionID
-    """
-    regionID = models.IntegerField(db_index=True)
-    
-    region_name = models.CharField(max_length=200, blank=True)
-    
-    #media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
-    
-    def __unicode__(self):
-        name = unicode(self.regionID)
-        if self.region_name:
-            name = self.region_name
-        return u'%s_%s' % (name, self.media)
-    
-    
-# Create your models here.
-#class Tag(models.Model):
-#    """
-#    Movie: tags
-#    MovieClip: tags
-#    Clip: tags
-#    """
-#    tags = models.CharField(max_length=200, blank=True, db_index=True)
-#    
-#    media = models.CharField(max_length=20, choices=GOTV_MEDIA_TYPE, db_index=True)
-#    
-#    def __unicode__(self):
-#        return u'%s_%s' % (self.tags, self.media)
-    
+    #    return ('movie',(),{'source_type':self.source_type,'id':self.movie_id})
 
-#class TopicID(models.Model):
-#    """
-#    MovieClip
-#    """
-#    topicID = models.IntegerField(db_index=True)
-#    
-#    topic_name = models.CharField(max_length=200, blank=True)
-#    
-#    def __unicode__(self):
-#        name = unicode(self.topicID)
-#        if self.topic_name:
-#            name = self.topic_name
-#        return u'%s' % (name)
+    class Meta:
+        app_label = 'movies'
+
+
+
+#TO DO
+#    fix get_or_create function
+
+if __name__ == "__main__":
+    a = Genre(genre_id=1,genre_name="a")
+    a.genre_name="test"
+    a.save()
+    b= Genre.objects.all()
+    print b[0]
+
+
